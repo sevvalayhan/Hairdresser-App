3d321512674d79dd9154cb90b85b60e7fa536c40
Sevval Ayhan <sevvalayhan2002@gmail.com>
2024-10-18 18:54:55 +0300

    MediaType enum and PostMedia class added

--- hairdresser_project/lib/models/post.dart
+++ hairdresser_project/lib/models/post.dart
@@ -1,22 +1,22 @@
 import 'package:hairdresser_project/models/hairdresser.dart';
+import 'package:hairdresser_project/models/post_media.dart';
+
 
 class Post {
   late int? id;
-  late Hairdresser hairdresser;
+  final Hairdresser hairdresser;
   final String content;
   final String categoryName;
-  String? videoUrl;
-  List<String>? postImageUrlList;
   late DateTime? createdAt;
   late DateTime? updatedAt;
+  late List<PostMedia>? postMediaList;
 
-  Post({
+  Post(
+     {this.postMediaList,
     required this.hairdresser,
     required this.categoryName,
     this.id,
     required this.content,
-    this.videoUrl,
-    this.postImageUrlList,
     this.createdAt,
     this.updatedAt,
   });
@@ -24,25 +24,34 @@ class Post {
   factory Post.fromJson(Map<String, dynamic> json) {
     return Post(
       id: json['id'],
+      postMediaList: json['postMediaList'] != null
+          ? (json['postMediaList'] as List)
+              .map((item) => PostMedia.fromJson(item))
+              .toList()
+          : null,
       hairdresser: Hairdresser.fromJson(json['hairdresser']),
+      categoryName: json['categoryName'],      
       content: json['content'],
-      categoryName: json['categoryName'],
-      videoUrl: json['videoUrl'],
-      postImageUrlList: json['postImage'],
-      createdAt: DateTime.parse(json['createdAt']),
-      updatedAt: DateTime.parse(json['updatedAt']),
+      createdAt: json['createdAt'] != null
+          ? DateTime.parse(json['createdAt'])
+          : null,
+      updatedAt: json['updatedAt'] != null
+          ? DateTime.parse(json['updatedAt'])
+          : null,
     );
   }
+
   Map<String, dynamic> toJson() {
     return {
       'id': id,
       'hairdresser': hairdresser.toJson(),
       'content': content,
       'categoryName': categoryName,
-      'videoUrl': videoUrl,
-      'postImage': postImageUrlList,
-      'createdAt': createdAt!.toIso8601String(),
-      'updatedAt': updatedAt!.toIso8601String(),
+      'createdAt': createdAt?.toIso8601String(),
+      'updatedAt': updatedAt?.toIso8601String(),
+      'postMedia': postMediaList,
     };
   }
 }
+
+enum MediaType { image, video }
