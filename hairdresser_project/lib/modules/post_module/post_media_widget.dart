fffad4e677793beadfdb44d30d337b0ca3a0e4ad
Sevval Ayhan <sevvalayhan2002@gmail.com>
2024-10-21 11:40:35 +0300

    Added ChewieController and VideoController to PostMediaWidge Class

--- hairdresser_project/lib/modules/post_module/post_media_widget.dart
+++ hairdresser_project/lib/modules/post_module/post_media_widget.dart
@@ -23,6 +23,13 @@ class _PostMediaWidgetState extends State<PostMediaWidget> {
   ChewieController? _chewieController;
 
   @override
+  void dispose() {
+    _videoPlayerController?.dispose();
+    _chewieController?.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     {
       return Column(
@@ -34,15 +41,19 @@ class _PostMediaWidgetState extends State<PostMediaWidget> {
                   controller: _pageController,
                   itemCount: widget.post.postMediaList!.length,
                   itemBuilder: (context, index) {
-                    if (widget.post.postMediaList?[index].mediaType == MediaType.video) {
-                      _videoPlayerController = VideoPlayerController.networkUrl(
-                          Uri.parse(widget.post.postMediaList![index].mediaUrl));
-                      _chewieController = ChewieController(
+                    if (widget.post.postMediaList?[index].mediaType ==
+                        MediaType.video) {
+                      String url = widget.post.postMediaList![index].mediaUrl;
+                      _videoPlayerController =
+                          VideoPlayerController.networkUrl(Uri.parse(url));
+                      //return CustomVideoPlayer(videoPlayerController: _videoPlayerController!);
+
+                       _chewieController = ChewieController(
                         videoPlayerController: _videoPlayerController!,
-                        autoPlay: true,
+                        autoPlay: false,
                         looping: true,
                       );
-                      return FutureBuilder(
+                       return FutureBuilder(
                           future: _videoPlayerController!.initialize(),
                           builder: (context, snapshot) {
                             if (snapshot.connectionState ==
@@ -53,7 +64,8 @@ class _PostMediaWidgetState extends State<PostMediaWidget> {
                                   child: CircularProgressIndicator());
                             }
                           });
-                    } else if (widget.post.postMediaList![index].mediaType == MediaType.image) {
+                    } else if (widget.post.postMediaList![index].mediaType ==
+                        MediaType.image) {
                       return CachedNetworkImage(
                         imageUrl: widget.post.postMediaList![index].mediaUrl,
                         progressIndicatorBuilder:
@@ -108,3 +120,33 @@ class _AnimatedProgressIndicatorState extends State<AnimatedProgressIndicator> {
     );
   }
 }
+
+// ignore: must_be_immutable
+class CustomVideoPlayer extends StatefulWidget {
+   CustomVideoPlayer({super.key,required this.videoPlayerController});
+  VideoPlayerController videoPlayerController;
+
+  @override
+  State<CustomVideoPlayer> createState() => _CustomVideoPlayerState();
+}
+
+class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
+Widget build(BuildContext context) {
+  return Column(
+    children: [
+      VideoPlayer(widget.videoPlayerController),
+      IconButton(
+        icon: Icon(
+          widget.videoPlayerController.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        ),
+        onPressed: () {
+          setState(() {
+            widget.videoPlayerController.value.isPlaying
+                ? widget.videoPlayerController.pause()
+                : widget.videoPlayerController.play();
+          });
+        },
+      ),
+    ],
+  );
+}}
