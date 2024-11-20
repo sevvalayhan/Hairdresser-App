dbd9ba6c6322af779492d11ea36dd013ab1fb67c
Sevval Ayhan <sevvalayhan2002@gmail.com>
2024-11-20 15:47:22 +0300

    Utf8 json decode method added

--- hairdresser_project/lib/repositories/service_repository.dart
+++ hairdresser_project/lib/repositories/service_repository.dart
@@ -3,79 +3,66 @@ import 'package:hairdresser_project/models/service.dart';
 import 'package:http/http.dart' as http;
 
 class ServiceRepository {
-  final String baseUrl = 'http://127.0.0.1:8000/barber/get-service';
+  final String baseUrl = 'http://127.0.0.1:8000/barber/';
 
-  Future<List<Service>> fetchAllServices() async {
-    print("-------------------");
+  Future<List<Service>?> fetchAllServices() async {
+    final url = Uri.parse('${baseUrl}get-service'); // API endpoint
     try {
-  final response = await http.get(Uri.parse("${baseUrl}"));
-  if (response.statusCode == 200) {
-    final data = jsonDecode(response.body); // veya kendi işlem yapınıza göre kullanın
-    print(data);
-  } else {
-    print("Başarısız istek: ${response.statusCode}");
-  }
-} catch (e) {
-  print("Hata: $e");
-}
+      final response = await http.get(url);
 
-
-    final response = await http.get(Uri.parse("${baseUrl}get-service"));
-    print(response);
-    print(response.body);
-    print(response.statusCode);
-    if (response.statusCode == 200) {
-      var fetchedServiceData =
-          jsonDecode(utf8.decode(response.bodyBytes)) as List<dynamic>;
-      List<Service> fechedServices =
-          fetchedServiceData.map((s) => Service.fromJson(s)).toList();
-      return fechedServices;
-    } else {
-      throw Exception((e) => "Failed to fetch services: ${e}");
-    }
-  }
-
-  Future<Service?> addService(Service service) async {
-    try {
-      final response = await http.post(
-        Uri.parse(baseUrl),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: json.encode(service.toJson()),
-      );
-      if (response.statusCode == 201) {
-        return Service.fromJson(json.decode(response.body));
-      } else {
-        print("Failed to add post");
-        return null;
-      }
-    } catch (e) {
-      print("Error adding post: $e");
-      return null;
-    }
-  }
-
-  Future<Service?> updateService(int serviceId, Service service) async {
-    try {
-      final response = await http.put(
-        Uri.parse('$baseUrl/$serviceId/'),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: json.encode(service.toJson()),
-      );
       if (response.statusCode == 200) {
-        return Service.fromJson(json.decode(response.body));
+        final List<dynamic> jsonData =
+            jsonDecode((utf8.decode(response.bodyBytes)));
+        return jsonData.map((service) => Service.fromJson(service)).toList();
       } else {
-        print("Failed to update post");
-        return null;
+        throw Exception('Failed to load services');
       }
-    } catch (e) {
-      print("Error updating post: $e");
-      return null;
+    } catch (error) {
+      throw Exception('Error fetching services: $error');
     }
   }
+
+  // Future<Service?> addService(Service service) async {
+  //   try {
+  //     final response = await http.post(
+  //       Uri.parse(baseUrl),
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: json.encode(service.toJson()),
+  //     );
+  //     if (response.statusCode == 201) {
+  //       return Service.fromJson(json.decode(response.body));
+  //     } else {
+  //       print("Failed to add post");
+  //       return null;
+  //     }
+  //   } catch (e) {
+  //     print("Error adding post: $e");
+  //     return null;
+  //   }
+  // }
+
+  // Future<Service?> updateService(int serviceId, Service service) async {
+  //   try {
+  //     final response = await http.put(
+  //       Uri.parse('$baseUrl/$serviceId/'),
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: json.encode(service.toJson()),
+  //     );
+  //     if (response.statusCode == 200) {
+  //       return Service.fromJson(json.decode(response.body));
+  //     } else {
+  //       print("Failed to update post");
+  //       return null;
+  //     }
+  //   } catch (e) {
+  //     print("Error updating post: $e");
+  //     return null;
+  //   }
+  // }
 
   Future<bool> deleteService(int serviceId) async {
     try {
